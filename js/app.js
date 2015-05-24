a147c78ab8b825f574d4ca65fd5a2e401f2ab99b
Arandi Lopez <arandilopez.93@gmail.com>
2015-05-24 12:10:51 -0500

    some comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-    moment.locale('es');
-  load_feed();
+    moment.locale('es'); // set locale to spanish
+    load_feed(); // load at start
 
-  $('#reload-btn').on('click', function(event) {
+    /**
+     * If press button, reload
+     */
+    $('#reload-btn').on('click', function(event) {
       event.preventDefault();
       load_feed();
-  });
+    });
 });
 
+/**
+ * This load the rss feed in rss-feed element.
+ * @return {[type]} [description]
+ */
 function load_feed() {
-    $('#rss-feed').html('');
+    $('#rss-feed').html(''); // clean content
     $('#rss-feed').rss("http://yucatan.com.mx/feed", {
+        // This is the single entry template
       entryTemplate: `
       <div class="collection-item">
           <span class="title"><h5><a href="{url}">{title}</a></h5></span>
@@ -18,14 +26,15 @@ function load_feed() {
           {shortBody}
       </div>
       `,
+      // template of container
       layoutTemplate: `
       <div class="collection">
             {entries}
       </div>
       `,
-      limit: 10,
-      dateFormat: 'MMMM D, YYYY',
-      effect: 'slideFastSynced',
-      ssl: true
+      limit: 10, // how many entries we want to display
+      dateFormat: 'MMMM D, YYYY', // format of date in entry
+      effect: 'slideFastSynced', // just an animation
+      ssl: true // use https to query and parse XML feed [required by chrome api]
     });
 }
